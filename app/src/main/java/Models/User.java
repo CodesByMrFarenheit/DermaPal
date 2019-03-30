19f03eaeed305fb8670d1ce48493e60e5bc3e9b2
Dhananjay <chobhedhananjay@gmail.com>
2019-03-30 15:45:26 +0530

    Backend Gui

--- app/src/main/java/Models/User.java
+++ app/src/main/java/Models/User.java
@@ -2,7 +2,74 @@ package Models;
 
 public class User {
 
-    
+    String userEmail;
+    String userName;
+    String userGender;
+    String userAge;
+    String userPhone;
+
+    public User() {
+        //for firebase
+    }
+
+    public User(String userEmail, String userName, String userGender, String userAge, String userPhone) {
+        this.userEmail = userEmail;
+        this.userName = userName;
+        this.userGender = userGender;
+        this.userAge = userAge;
+        this.userPhone = userPhone;
+    }
 
 
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserGender() {
+        return userGender;
+    }
+
+    public void setUserGender(String userGender) {
+        this.userGender = userGender;
+    }
+
+    public String getUserAge() {
+        return userAge;
+    }
+
+    public void setUserAge(String userAge) {
+        this.userAge = userAge;
+    }
+
+    public String getUserPhone() {
+        return userPhone;
+    }
+
+    public void setUserPhone(String userPhone) {
+        this.userPhone = userPhone;
+    }
+
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userEmail='" + userEmail + '\'' +
+                ", userName='" + userName + '\'' +
+                ", userGender='" + userGender + '\'' +
+                ", userAge='" + userAge + '\'' +
+                ", userPhone='" + userPhone + '\'' +
+                '}';
+    }
 }
--- app/src/main/java/com/sacredcodes/dermapal/MainActivity.java
+++ app/src/main/java/com/sacredcodes/dermapal/MainActivity.java
@@ -1,17 +1,280 @@
 package com.sacredcodes.dermapal;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import Models.User;
 
 public class MainActivity extends AppCompatActivity {
+    //Firebase
+    private FirebaseAuth mAuth;
+    private DatabaseReference mDatabase;
+
+
+    // Layouts
+    ConstraintLayout layoutMain;
+    ConstraintLayout layoutSignIn;
+    ConstraintLayout layoutSignUp;
+    ConstraintLayout layoutReg;
+
+    //main Layout
+
+    Button signInButton;
+    Button signUpButton;
+
+
+    //Sign In Layout
+    Button signInButton2;
+    EditText userEmail;
+    EditText userPassword;
+
+    //Sign Up layout
+    EditText userEmailSignUp;
+    EditText userPasswordSignUp;
+    Button goToRegButton;
+
+    //Reg Layout
+    ImageButton genderMale;
+    ImageButton GenderFemale;
+    Button SignUpButton2;
+    EditText userName;
+    EditText userPhone;
+    EditText userAge;
+    String userGender;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mAuth = FirebaseAuth.getInstance();
 
 
+
+
+        signInButton = findViewById(R.id.signInButton);
+        signUpButton = findViewById(R.id.signUpButton);
+        layoutMain = findViewById(R.id.layoutMain);
+        layoutSignIn = findViewById(R.id.layoutSignIn);
+        layoutSignUp = findViewById(R.id.layoutSignUp);
+        layoutReg = findViewById(R.id.layoutReg);
+
+        //Sign In Layout
+        signInButton2 = findViewById(R.id.signInButton2);
+        userEmail =  findViewById(R.id.userEmail);
+        userPassword = findViewById(R.id.userPassword);
+
+        //Sign Up layout
+        userEmailSignUp = findViewById(R.id.userEmailSignUp);
+        userPasswordSignUp =  findViewById(R.id.userPasswordSignUp);
+        goToRegButton = findViewById(R.id.goToRegButton);
+
+        //Reg Layout
+        genderMale =  findViewById(R.id.genderMale);
+        GenderFemale = findViewById(R.id.genderFemale);
+        SignUpButton2 =  findViewById(R.id.signUpButton2);
+        userName = findViewById(R.id.userName);
+        userPhone = findViewById(R.id.userPhone);
+        userAge = findViewById(R.id.userAge);
+
+
+        //Layout Initial Status
+        layoutMain.setVisibility(View.VISIBLE);
+        layoutSignIn.setVisibility(View.INVISIBLE);
+        layoutSignUp.setVisibility(View.INVISIBLE);
+        layoutReg.setVisibility(View.INVISIBLE);
+
+    genderMale.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+            userGender = "male";
+
+        }
+    });
+
+    GenderFemale.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+            userGender = "female";
+
+        }
+    });
+
+
+        SignUpButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String userName1= userName.getText().toString().trim();
+                String userEmail1 = userEmailSignUp.getText().toString().trim();
+                String userAge1 = userAge.getText().toString().trim();
+                String userPhone1 = userPhone.getText().toString().trim();
+                String userGender1 = userGender;
+                String userPassword1 = userPasswordSignUp.getText().toString().trim();
+
+                User  user = new User(userEmail1, userName1, userGender1, userAge1, userPhone1);
+
+                signUpFirebase(user, userPassword1);
+            }
+        });
+
+
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                layoutMain.setVisibility(View.GONE);
+                layoutSignUp.setVisibility(View.VISIBLE);
+            }
+        }
+    );
+
+    signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                layoutMain.setVisibility(View.GONE);
+                layoutSignIn.setVisibility(View.VISIBLE);
+            }
+        }
+    );
+
+    signInButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                layoutSignIn.setVisibility(View.GONE);
+                String userName1 = userEmail.getText().toString().trim();
+                String userPassword1 = userPassword.getText().toString().trim();
+
+                signInFirebase(userName1, userPassword1);
+
+
+            }
+        }
+    );
+
+
+    goToRegButton.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+            layoutSignUp.setVisibility(View.GONE);
+            layoutReg.setVisibility(View.VISIBLE);
+        }
+    });
+
+
+
+
+//End of OnCreate
+    }
+
+    private void signUpFirebase(final User user1, String password) {
+        mAuth.createUserWithEmailAndPassword(user1.getUserEmail(), password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            public static final String TAG = "Tag" ;
+
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+
+                    User user = new User(user1.getUserEmail(),user1.getUserName(), user1.getUserGender(), user1.getUserAge(), user1.getUserPhone());
+                    mDatabase= FirebaseDatabase.getInstance().getReference();
+                    FirebaseUser user2 = mAuth.getCurrentUser();
+                    mDatabase.child("Users").child(user2.getUid()).setValue(user);
+
+
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "createUserWithEmail:success");
+                    Toast.makeText(MainActivity.this, "Sign Up .",
+                            Toast.LENGTH_SHORT).show();
+
+                    updateUI(user2);
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                    Toast.makeText(MainActivity.this, "Authentication failed.",
+                            Toast.LENGTH_SHORT).show();
+                    updateUI(null);
+                }
+            }
+
+        });
+
+
+
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
+
+    private void signInFirebase(String email, String password) {
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    public static final String TAG = "Tag" ;
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+
+
+                    }
+
+                });
+
+
+
+    }
+
+    private void updateUI(FirebaseUser user) {
+
+
+        if(user != null)
+        {
+
+            //String userEmail= user.getEmail();
+            Intent i = new Intent(this, HomeActivity.class);
+            //i.putExtra("userEmail", userEmail);
+            startActivity(i);
+        }
 
 
     }
